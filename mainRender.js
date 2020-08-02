ffad4d2794791d92ecf3419645fe1d1fa22fd29a
iComputeDaily <henry.haines@thetwelfth.com>
2020-08-02 12:45:47 -0400

    Improve formatting add comments and make the canvas element fullscreen with no scrollbar.

--- mainRender.js
+++ mainRender.js
@@ -1,12 +1,14 @@
 function main() {
-	const canvas = document.querySelector("#glCanvas");
-	const gl = canvas.getContext("experimental-webgl");
+	const canvas = document.querySelector("#glCanvas"); // Find the element we want to draw to
+	const gl = canvas.getContext("experimental-webgl"); // Get the context
 
+	// Exit if something went worng
 	if (gl === null) {
 		alert("Unable to initialize WebGL. Your browser or machine may not support it.");
 		return;
 	}
 
+	// Set the color to clear the buffer with and then clear the buffer
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 }
